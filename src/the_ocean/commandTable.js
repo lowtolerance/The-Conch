44fecf4c3c37d360455672b1f0c8321c71776c61
Rob Nixon <robnixon515@gmail.com>
2018-04-26 12:08:16 -0400

    fleshed out commandTable placeholder values

--- src/the_ocean/commandTable.js
+++ src/the_ocean/commandTable.js
@@ -1,8 +1,19 @@
+/* This is just a sort of functional placeholder. Ultimately, all
+available commands will be stored in a cloud-based database. When
+a device is added, we query the database for all of the relevant
+information, which then gets added to our local storage. */
+
 const commandTable = {
   input: function (command) {
     const commands = {
-      A90: function () {
+      'A90': function () {
         return 'TV_POWER_TOGGLE'
+      },
+      '890': function () {
+        return 'TV_VOLUME_UP'
+      },
+      'B90': function () {
+        return 'TV_VOLUME_DOWN'
       },
       default: function () {
         return 'UNDEFINED'
@@ -12,8 +23,14 @@ const commandTable = {
   },
   output: function (command) {
     const commands = {
-      TV_POWER_TOGGLE: function () {
+      'TV_POWER_TOGGLE': function () {
         return 'echo as 0 | cec-client -s1 -d'
+      },
+      'TV_VOLUME_UP': function () {
+        return '45ac3a'
+      },
+      'TV_VOLUME_DOWN': function () {
+        return '45ab6e'
       },
       default: function () {
         return 'UNDEFINED'
