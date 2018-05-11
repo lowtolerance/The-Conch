12eaae1f9d452737ae8c3bf3a9b0de7642203082
Rob Nixon <robnixon515@gmail.com>
2018-05-11 16:26:18 -0400

    toggle device manager

---
+++ src/utils/index.js
@@ -0,0 +1,11 @@
+import commandTable from '../models/commandTable'
+
+const _mapI2U = command => {
+  return commandTable.input(command).value
+}
+
+const _mapU2O = command => {
+  return commandTable.output(command).value
+}
+
+export const map = command => _mapU2O(_mapI2U(command))
