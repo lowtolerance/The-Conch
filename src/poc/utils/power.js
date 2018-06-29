a33442c0fa2b739ca2ff8fcdd71705ac74236301
Rob Nixon <robnixon515@gmail.com>
2018-06-29 14:11:39 -0400

    Power module export logic was all fucked up

--- src/poc/utils/power.js
+++ src/poc/utils/power.js
@@ -1,46 +1,45 @@
 const util = require('util')
 const exec = util.promisify(require('child_process').exec)
 
-async function on () {
-  const { stdout, stderr } = await exec(
-    'echo on 0 | cec-client RPI -s -d 1'
-  )
-  if (stderr) {
-    console.log(stderr)
-  }
-  console.log(stdout)
-}
+const power = {
+  async on () {
+    const { stdout, stderr } = await exec(
+      'echo on 0 | cec-client RPI -s -d 1'
+    )
+    if (stderr) {
+      console.log(stderr)
+    }
+    console.log(stdout)
+  },
+  async off () {
+    const { stdout, stderr } = await exec(
+      'echo standby 0 | cec-client RPI -s -d 1'
+    )
+    if (stderr) {
+      console.log(stderr)
+    }
+    console.log(stdout)
+  },
 
-async function off () {
-  const { stdout, stderr } = await exec(
-    'echo standby 0 | cec-client RPI -s -d 1'
-  )
-  if (stderr) {
-    console.log(stderr)
+  async toggle () {
+    const power = await this.getStatus()
+    if (power) {
+      this.off()
+    } else this.on()
+  },
+  async getStatus () {
+    const { stdout, stderr } = await exec(
+      'echo pow 0 | cec-client RPI -s -d 1'
+    )
+    if (stderr) {
+      console.log(stderr)
+    }
+    if (stdout.includes('standby')) {
+      return false
+    } else if (stdout.includes('on')) {
+      return true
+    }
+    console.log(stdout)
   }
-  console.log(stdout)
 }
-
-async function toggle () {
-  const power = await getStatus()
-  if (power) {
-    off()
-  } else on()
-}
-async function getStatus () {
-  const { stdout, stderr } = await exec(
-    'echo pow 0 | cec-client RPI -s -d 1'
-  )
-  if (stderr) {
-    console.log(stderr)
-  }
-  if (stdout.includes('standby')) {
-    return false
-  } else if (stdout.includes('on')) {
-    return true
-  }
-  console.log(stdout)
-}
-
-module.exports = getStatus
-module.exports = toggle
+export default power
