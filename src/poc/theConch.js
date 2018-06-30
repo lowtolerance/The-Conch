1d4e735c48d3027dc13e5d50bed86ac81657845e
Rob Nixon <lowtolerance@users.noreply.github.com>
2018-06-30 09:00:21 -0400

    cleaned up theConch module by a lot

--- src/poc/theConch.js
+++ src/poc/theConch.js
@@ -2,18 +2,6 @@ const enqueue = require('./utils/enqueue')
 const dequeue = require('./utils/dequeue')
 const lookup = require('./lookup')
 
-// Iterate through our event queue until no events
-// remain. Susceptible to being replaced by a
-// different strategy if the need arises.
-
-function runout () {
-  while (this.eventQueue.length !== 0) {
-    const event = dequeue() // Get event
-    lookup(event) // Act accordingly
-  }
-  console.timeEnd() // end timer. prints time from start to finish upon receiving an event.
-}
-
 // Takes socket.io socket as input,
 // which it probably shouldn't.
 // socket logic in general is likely
@@ -21,13 +9,16 @@ function runout () {
 // drastically
 
 function theConch (openSocket) {
-  global.eventQueue = [] // Init queue
+  global.eventQueue = []
   openSocket.on('connect', connected => {
     connected.on('TC', data => {
-      console.time() // Start timer
-      console.log(`Caught signal '${data}'`)
-      enqueue(data) // Add socket message payload to event queue
-      runout() // Run out our queue.
+      console.time()
+      enqueue(data)
+      while (this.eventQueue.length !== 0) {
+        const event = dequeue()
+        lookup(event)
+      }
+      console.timeEnd()
     })
   })
 }
